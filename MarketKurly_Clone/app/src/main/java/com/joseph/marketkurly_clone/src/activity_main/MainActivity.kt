b075af280b3dcc959d59a0d5ac3e780cb071844d
zoey <zooey4u@gmail.ccom>
2020-11-01 16:25:39 +0900

    # BottomNavigation 구현

--- MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_main/MainActivity.kt
+++ MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_main/MainActivity.kt
@@ -2,25 +2,48 @@ package com.joseph.marketkurly_clone.src.activity_main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.joseph.marketkurly_clone.R
 import com.joseph.marketkurly_clone.src.activity_main.interfaces.MainActivityValidation
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), MainActivityValidation {
+class MainActivity : AppCompatActivity(),
+    BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        initActivity()
     }
 
-    override fun validateSuccess(text: String?) {
-        // 인증 성공 시
-        TODO("Not yet implemented")
+    fun initActivity () {
+        bottomNavigationView.setupWithNavController(findNavController(R.id.nav_host_fragment_container))
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
     }
 
-    override fun validateFailure(message: String?) {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        when(item.title) {
+            "홈" -> {
+                navController.navigate(R.id.homeFragment)
+            }
+            "카테고리" ->{
+                navController.navigate(R.id.categoryFragment)
+            }
+            "추천" -> {
+                navController.navigate(R.id.recommendFragment)
+            }
+            "검색" -> {
+                navController.navigate(R.id.searchFragment)
+            }
+            "마이컬리" -> {
+                navController.navigate(R.id.myKurlyFragment)
+            }
+        }
 
-        // 인증 실패 시
-        TODO("Not yet implemented")
+        return true
     }
 }
