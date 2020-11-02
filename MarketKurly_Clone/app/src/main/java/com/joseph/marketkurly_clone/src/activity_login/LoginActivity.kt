4ea6ad759516ce0406c1dc964124a07672e74e00
zoey <zooey4u@gmail.ccom>
2020-11-02 16:45:37 +0900

    # LoginActivity 구현중 (10%)

---
+++ MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.joseph.marketkurly_clone.src.activity_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joseph.marketkurly_clone.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
