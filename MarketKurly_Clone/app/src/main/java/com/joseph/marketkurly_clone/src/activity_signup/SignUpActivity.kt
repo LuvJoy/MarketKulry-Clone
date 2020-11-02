c5aeec89fef9576f2528098074daa5fa1981324c
zoey <zooey4u@gmail.ccom>
2020-11-03 02:24:27 +0900

    # SignUpActivity UI 구현중 (90%)
    - 스크롤 시 액션바 처리하기 (앱 전체에 액션바를 어떻게 처리할지 고민해야함)
    - 유효성 검사 해야함

--- MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_signup/SignUpActivity.kt
+++ MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_signup/SignUpActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.View
 import com.joseph.marketkurly_clone.BaseActivity
 import com.joseph.marketkurly_clone.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
     }
 
     override fun onClick(view: View?) {
