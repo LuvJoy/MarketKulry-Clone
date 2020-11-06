fbaaa2a75045f24ba314fb7677aaff8613f29269
zoey <zooey4u@gmail.ccom>
2020-11-06 16:09:20 +0900

    # 제품 상세화면 구현중 [40%]

--- MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_detail_product/DetailProductActivity.kt
+++ MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_detail_product/DetailProductActivity.kt
@@ -7,6 +7,8 @@ import com.google.android.material.tabs.TabLayoutMediator
 import com.joseph.marketkurly_clone.R
 import com.joseph.marketkurly_clone.src.activity_detail_product.adapters.DetailTabLayoutAdapter
 import com.joseph.marketkurly_clone.src.activity_main.fragments.frag_home.adapters.TabViewPagerAdapter
+import com.joseph.marketkurly_clone.src.util.setGone
+import kotlinx.android.synthetic.main.actionbar_inner_page_top.view.*
 import kotlinx.android.synthetic.main.activity_detail_product.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -15,9 +17,15 @@ class DetailProductActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_product)
 
+        initActionBar()
         initViewPager()
     }
 
+    fun initActionBar() {
+        detail_actionbar.ab_inner_div_line.setGone()
+        detail_actionbar.ab_inner_toolbar.title = "제품 상세설명"
+        detail_actionbar.ab_inner_toolbar.setNavigationOnClickListener { onBackPressed() }
+    }
     fun initViewPager() {
         val fm = this.supportFragmentManager
         detail_viewpager.adapter = DetailTabLayoutAdapter(this)
--- MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_detail_product/fragments/frag_explain/ExplainFragment.kt
+++ MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_detail_product/fragments/frag_explain/ExplainFragment.kt
@@ -1,13 +1,25 @@
 package com.joseph.marketkurly_clone.src.activity_detail_product.fragments.frag_explain
 
+import android.graphics.drawable.RippleDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.joseph.marketkurly_clone.R
+import kotlinx.android.synthetic.main.fragment_detail_tab_explain.*
 
 class ExplainFragment : Fragment(R.layout.fragment_detail_tab_explain) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(this)
+            .load(R.drawable.dummy_image2)
+            .into(detail_explain_product_imageview)
+
+    }
 
 
 }
