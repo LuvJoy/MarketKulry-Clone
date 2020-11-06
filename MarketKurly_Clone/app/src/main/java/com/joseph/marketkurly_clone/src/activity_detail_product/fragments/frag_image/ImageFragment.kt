94ec9675da1dea3455608a6e0eedf7667566eb8e
zoey <zooey4u@gmail.ccom>
2020-11-06 22:11:12 +0900

    # 제품 상세화면 구현중 [85%] -> 이미지 확대기능 구현완료

--- MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_detail_product/fragments/frag_image/ImageFragment.kt
+++ MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_detail_product/fragments/frag_image/ImageFragment.kt
@@ -5,8 +5,10 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.joseph.marketkurly_clone.R
 import com.joseph.marketkurly_clone.src.activity_image_viewer.ImageViewerActivity
 import kotlinx.android.synthetic.main.fragment_detail_tab_image.*
 import java.io.ByteArrayOutputStream
@@ -23,18 +25,13 @@ class ImageFragment : Fragment(R.layout.fragment_detail_tab_image), View.OnClick
             .load("https://sub.marketkulry.shop/img/product1_detail.png")
             .into(detail_image_imageview)
 
-        var a = detail_image_imageview.drawable
-
-        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_launcher)
-        val baos = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos)
-        val b: ByteArray = baos.toByteArray()
     }
 
     override fun onClick(v: View?) {
         when(v?.id) {
             R.id.detail_image_imageview -> {
                 val intent = Intent(requireContext(), ImageViewerActivity::class.java)
+                intent.putExtra("url", "https://sub.marketkulry.shop/img/product1_detail.png")
                 startActivity(intent)
             }
         }
--- MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_image_viewer/ImageViewerActivity.kt
+++ MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_image_viewer/ImageViewerActivity.kt
@@ -1,21 +1,25 @@
 package com.joseph.marketkurly_clone.src.activity_image_viewer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
+import com.bumptech.glide.Glide
 import com.davemorrissey.labs.subscaleview.ImageSource
 import com.joseph.marketkurly_clone.BaseActivity
 import com.joseph.marketkurly_clone.R
 import kotlinx.android.synthetic.main.activity_image_viewer.*
 
+
 class ImageViewerActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_viewer)
-
-        val imgUrl = intent.extras?.getString("imageUrl")
-
-        imageviewer_scale_imageview.setImage(ImageSource.resource(R.drawable.dummy_image2))
+        imageviewer_close_button.setOnClickListener(this)
+        val url = intent.extras?.getString("url")
+        Glide.with(this)
+            .load(url)
+            .into(imageviewer_scale_imageview)
+        // .setImage(ImageSource.resource(R.drawable.dummy_image2))
     }
 
     override fun onClick(v: View?) {
