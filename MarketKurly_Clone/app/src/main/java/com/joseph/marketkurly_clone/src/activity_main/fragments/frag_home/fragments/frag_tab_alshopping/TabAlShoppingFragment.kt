b74e2b4fd86fea8f592d5a5660f2f5a6201aa626
zoey <zooey4u@gmail.ccom>
2020-11-11 00:09:17 +0900

    # HOME -> 알뜰상품탭 구현

--- MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_main/fragments/frag_home/fragments/frag_tab_alshopping/TabAlShoppingFragment.kt
+++ MarketKurly_Clone/app/src/main/java/com/joseph/marketkurly_clone/src/activity_main/fragments/frag_home/fragments/frag_tab_alshopping/TabAlShoppingFragment.kt
@@ -5,9 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.joseph.marketkurly_clone.BaseActivity
 import com.joseph.marketkurly_clone.BaseFragment
 import com.joseph.marketkurly_clone.R
+import com.joseph.marketkurly_clone.src.activity_main.fragments.frag_home.fragments.adapters.ProductRecyclerAdapter
+import com.joseph.marketkurly_clone.src.activity_main.fragments.frag_home.fragments.frag_tab_reckurly.HomeProductService
+import com.joseph.marketkurly_clone.src.activity_main.fragments.frag_home.fragments.frag_tab_reckurly.interfaces.HomeProductApiEvent
+import com.joseph.marketkurly_clone.src.activity_main.fragments.frag_home.models.ProductCompact
+import kotlinx.android.synthetic.main.fragment_tab_al_shopping.*
+import kotlinx.android.synthetic.main.fragment_tab_best.*
+import kotlinx.android.synthetic.main.fragment_tab_best.tab_best_product_recyclerview
 
-class TabAlShoppingFragment : BaseFragment(R.layout.fragment_tab_al_shopping) {
+class TabAlShoppingFragment : BaseFragment(R.layout.fragment_tab_al_shopping), HomeProductApiEvent {
 
+    private lateinit var mAlShoppingProductRecyclerAdapter: ProductRecyclerAdapter
+    private var mProductSevice = HomeProductService(this)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+        mProductSevice.getRecommendProduct()
+    }
+
+    fun initRecyclerView() {
+
+        mAlShoppingProductRecyclerAdapter = ProductRecyclerAdapter(fragContext!!)
+
+        tab_al_shopping_product_recyclerview.apply {
+            adapter = mAlShoppingProductRecyclerAdapter
+            layoutManager = object :
+                GridLayoutManager(fragContext, 2, GridLayoutManager.VERTICAL, false) {
+                override fun canScrollVertically(): Boolean {
+                    return false
+                }
+            }
+            setHasFixedSize(false)
+            isNestedScrollingEnabled = false
+        }
+    }
+
+    override fun onProductLoadSuccess(list: ArrayList<ProductCompact>) {
+        list.shuffle()
+        mAlShoppingProductRecyclerAdapter.submitList(list)
+
+    }
+
+    override fun onProductLoadFail(message: String) {
+        val ac = activity as BaseActivity
+        ac.showSnackBar(message)
+    }
 }
